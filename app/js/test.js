05a4cae744c7dbdd7e890dbe583f3e14b6b9168b
Julie Manhes <julie.manhes@hetic.net>
2018-06-13 19:56:59 +0200

    add many things

--- app/js/test.js
+++ app/js/test.js
@@ -1,4 +1,10 @@
-console.log('Hello');
+// ADD //
+
+var addIcon = document.querySelector('.header__add-container');
+var addMenu = document.querySelector('.add_card');
+
+addIcon.addEventListener('click', function() {
+	addMenu.classList.toggle('hidden');
 
 
-console.log($('h1'));
+});
