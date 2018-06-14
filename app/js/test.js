31b24bd8e26d0f4359ad1c913efead0fe41c399b
Julie Manhes <julie.manhes@hetic.net>
2018-06-14 16:07:41 +0200

    add tags

--- app/js/test.js
+++ app/js/test.js
@@ -7,3 +7,17 @@ addIcon.addEventListener('click', function() {
 	addMenu.classList.toggle('hidden');
 
 });
+
+
+// ADD TAGG //
+
+var addTag = document.querySelector('.tags__add-tag a');
+var section = document.querySelector('.contacts__list');
+
+addTag.addEventListener('click', function(e) {
+	e.preventDefault();
+	var newArticle = document.createElement('article');
+	newArticle.className = "contactCard add_submit"
+	section.appendChild(newArticle);
+	newArticle.innerHTML = '<div class="container-tag"><form action="formulaire_submit" method="get" accept-charset="utf-8"><div class="tagsCard__main-infos"><input type="text"></div><div class="tagsCard__explain"><input type="text" placeholder="Saisissez une description..."></div><div class="tagsCard__uses"><p>La couleur du tag sera ajoutée aléatoirement</p></div><div class="tags__submit-tag"><input type="submit" value="Ajouter un tag"></div></form></div>';
+});
